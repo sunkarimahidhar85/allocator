a11d3bfe121793344553248ba36ff39526d1b15b
cfregly <chris@fregly.com>
2017-12-01 08:33:17 -0800

    udpated quota numbers

--- scripts/create_instances.sh
+++ scripts/create_instances.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Args
-#  $1: zone (us-west1-b (230), us-east1-d (50), europe-west1-b (50), asia-east1-a (50))
-#  $2: num_instances
+#   $1:  zone: (us-west1-b (230), us-east1-d (150), europe-west1-b (200), asia-east1-a (64))
+#   $2: num_instances
 
 for idx in `seq 1 $2`;
 do
