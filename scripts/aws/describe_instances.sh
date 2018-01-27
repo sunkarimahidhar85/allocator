b17062596c5ed3c0f006b6f0912f490a07f7064b
cfregly <chris@fregly.com>
2018-01-26 22:20:40 -0800

    remove ewave

--- scripts/aws/describe_instances.sh
+++ scripts/aws/describe_instances.sh
@@ -4,9 +4,9 @@
 #  $1: region
 #  $2: id
 
-length = $(aws ec2 describe-instances \
-    --filters 'Name=tag:pipelineai,Values=workshop' \
-    | jq length .Reservations[0].Instances)
+#length = $(aws ec2 describe-instances \
+#    --filters 'Name=tag:pipelineai,Values=workshop' \
+#    | jq length .Reservations[0].Instances)
 
 
 for idx in {0..length}; do
